04ec7453bd467fde8defbd9353faf182fe2e92d3
DannyHTML <dannyjag@gmail.com>
2023-07-05 13:59:51 +0200

    Made the layout complete. Now working on the job filtering

--- src/MainContent.vue
+++ src/MainContent.vue
@@ -2,12 +2,21 @@
   <!-- Searchbox -->
   <!-- For now, hidden -->
   <div
-    class="hidden justify-center border-2 rounded-lg border-none shadow-xl overflow-hidden max-w-7xl w-11/12 m-auto relative -top-8"
+    class="justify-center border-2 rounded-lg border-none shadow-xl overflow-hidden max-w-7xl w-11/12 m-auto relative -top-8"
   >
     <div class="relative w-full">
-      <input class="w-full h-16" type="search" name="" id="" />
+      <input
+        class="w-full h-16 pl-4 outline-none"
+        type="text"
+        name="search"
+        id="search"
+        v-model="selectJob"
+        @input="Input"
+        autocomplete="off"
+      />
       <button
-        class="absolute top-1/2 -translate-y-1/2 capitalize right-0 -translate-x-1/2 text-darkGrayishCyan font-bold"
+        class="absolute top-1/2 -translate-y-1/2 text-lg capitalize right-0 -translate-x-1/2 text-darkGrayishCyan hover:font-bold decoration-2 hover:cursor-pointer hover:underline hover:underline-offset-4"
+        @click="clearSearch"
       >
         clear
       </button>
@@ -15,62 +24,76 @@
   </div>
   <!-- End searchbox -->
   <div
-    class="relative border-2 mt-12 h-56 md:h-44 bg-white border-transparent max-w-7xl w-11/12 rounded-md m-auto shadow-xl"
+    v-for="item in items"
+    :key="item.id"
+    class="relative border-2 mt-12 h-64 md:h-44 bg-white border-transparent max-w-7xl w-11/12 rounded-md m-auto shadow-xl last:mb-12"
   >
     <div
       class="absolute md:-translate-y-1/2 md:top-1/2 md:translate-x-1/2 top-0 md:left-1 left-0 translate-x-5 -translate-y-1/2"
     >
       <img
         class="h-14 object-cover"
-        :src="currentImage"
-        :alt="currentImageAlt"
+        :src="getImageSrc(item.id)"
+        :alt="getImageId(item.id)"
       />
     </div>
     <div class="mt-10 md:ml-28 w-10/12 m-auto">
-      <div v-for="item in items" :key="item.id">
-        <div class="md:flex md:justify-between items-center">
-          <div>
-            <div class="flex gap-1 items-center mb-2">
-              <div class="mr-2 text-primary font-bold">{{ item.company }}</div>
+      <div class="md:flex md:justify-between items-center">
+        <div>
+          <div class="flex gap-2 items-center mb-2">
+            <div class="mr-2 text-primary font-bold">{{ item.company }}</div>
 
-              <div
-                class="uppercase bg-primary p-1 rounded-xl text-white"
-                v-if="item.new"
-              >
-                new!
-              </div>
-              <div
-                class="uppercase bg-veryDarkGrayishCyan rounded-xl p-1 text-white"
-                v-if="item.featured"
-              >
-                featured
-              </div>
+            <div
+              class="uppercase bg-primary p-1 rounded-xl text-white"
+              v-if="item.new"
+            >
+              new!
             </div>
-            <div class="text-black font-bold mb-2">
-              {{ item.position }}
+            <div
+              class="uppercase bg-veryDarkGrayishCyan rounded-xl p-1 text-white"
+              v-if="item.featured"
+            >
+              featured
             </div>
-            <div class="flex gap-8">
-              <div>{{ item.postedAt }}</div>
-              <div class="">
-                <ul class="list-disc">
-                  <li>{{ item.contract }}</li>
-                </ul>
-              </div>
-              <div>
-                <ul class="list-disc">
-                  <li>{{ item.location }}</li>
-                </ul>
-              </div>
-            </div>
-            <div class="mt-2 mb-2 border-b-2 md:border-b-0"></div>
           </div>
-          <div>
-            <div class="flex flex-wrap gap-2">
-              <div>{{ item.role }}</div>
-              <div>{{ item.level }}</div>
-              <div v-for="language in item.languages" :key="language">
-                {{ language }}
-              </div>
+          <div
+            class="text-black font-bold mb-2 hover:text-primary hover: cursor-pointer md:text-xl"
+          >
+            {{ item.position }}
+          </div>
+          <div class="flex gap-8">
+            <div>{{ item.postedAt }}</div>
+            <div>
+              <ul class="list-disc">
+                <li>{{ item.contract }}</li>
+              </ul>
+            </div>
+            <div>
+              <ul class="list-disc">
+                <li>{{ item.location }}</li>
+              </ul>
+            </div>
+          </div>
+          <div class="mt-2 mb-2 border-b-2 md:border-b-0"></div>
+        </div>
+        <div>
+          <div class="flex-wrap gap-4 flex text-primary font-bold">
+            <div
+              class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
+            >
+              {{ item.role }}
+            </div>
+            <div
+              class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
+            >
+              {{ item.level }}
+            </div>
+            <div
+              class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
+              v-for="language in item.languages"
+              :key="language"
+            >
+              {{ language }}
             </div>
           </div>
         </div>
@@ -80,10 +103,9 @@
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import data from "./assets/data.json";
 
-const currentIndex = ref(0);
 const images = [
   { src: "/images/photosnap.svg", alt: "image 1" },
   { src: "/images/manage.svg", alt: "image 2" },
@@ -93,17 +115,28 @@ const images = [
   { src: "/images/faceit.svg", alt: "image 6" },
   { src: "/images/shortly.svg", alt: "image 7" },
   { src: "/images/insure.svg", alt: "image 8" },
-  { src: "/images/eyecam.svg", alt: "image 9" },
+  { src: "/images/eyecam-co.svg", alt: "image 9" },
   { src: "/images/the-air-filter-company.svg", alt: "image 10" },
 ];
-
-const currentImage = computed(() => images[currentIndex.value].src);
-const currentImageAlt = computed(() => images[currentIndex.value].alt);
 
 const item = ref(data);
 const items = item.value;
 
-// new and featured values and booleans
+const getImageSrc = (id) => {
+  const imageIndex = id - 1;
+  return images[imageIndex].src;
+};
+
+const getImageId = (id) => {
+  const imageIndex = id - 1;
+  return images[imageIndex].src;
+};
+
+const selectJob = ref();
+
+const clearSearch = () => {
+  selectJob.value = "";
+};
 </script>
 
 <style scoped></style>
