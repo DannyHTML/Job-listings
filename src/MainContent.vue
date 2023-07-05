fb08ee6533746f2526c6d4bacf0b8f12ad0b639c
DannyHTML <dannyjag@gmail.com>
2023-07-05 17:53:49 +0200

    Testing some stuff

--- src/MainContent.vue
+++ src/MainContent.vue
@@ -12,7 +12,6 @@
           type="text"
           name="search"
           id="search"
-          v-model="selectJob"
           autocomplete="off"
         />
         <button
@@ -25,8 +24,24 @@
     </div>
   </Transition>
   <!-- End searchbox -->
+  <!-- Filtering tags -->
+  <div class="flex gap-4 mt-8 ml-28 border-2">
+    <div class="flex items-center" v-for="tag in selectedTags" :key="tag">
+      <div class="text-primary text-lg p-1 font-bold bg-slate-200">
+        {{ tag }}
+      </div>
+      <div
+        class="cursor-pointer rounded-sm bg-primary hover:bg-veryDarkGrayishCyan p-2"
+        @click="removeTag(tag)"
+      >
+        <img class="w-5" src="images/icon-remove.svg" alt="" />
+      </div>
+      <div class="item-self-end">Clear</div>
+    </div>
+  </div>
+  <!-- End filtering tags -->
   <div
-    v-for="item in items"
+    v-for="item in filteredItems"
     :key="item.id"
     class="relative border-2 border-transparent mt-12 h-64 md:h-44 bg-white max-w-7xl w-11/12 rounded-md m-auto shadow-xl last:mb-12"
     :class="{ 'border-l-4 border-l-primary': item.featured }"
@@ -79,17 +94,18 @@
           </div>
           <div class="mt-2 mb-2 border-b-2 md:border-b-0"></div>
         </div>
-        <div>
+        <div id="tags">
           <div class="flex-wrap gap-4 flex text-primary font-bold">
             <div
               class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
-              @click="addItem"
+              @click="toggleTag(item.role)"
             >
               {{ item.role }}
             </div>
 
             <div
               class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
+              @click="toggleTag(item.level)"
             >
               {{ item.level }}
             </div>
@@ -98,6 +114,7 @@
               class="bg-filterTabs p-1 rounded-md cursor-pointer hover:text-white hover:bg-primary"
               v-for="language in item.languages"
               :key="language"
+              @click="toggleTag(language)"
             >
               {{ language }}
             </div>
@@ -109,7 +126,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import data from "./assets/data.json";
 
 const images = [
@@ -149,6 +166,40 @@ const isAddItem = ref(true);
 const addItem = () => {
   isAddItem.value = !isAddItem.value;
 };
+
+// test test test
+
+const selectedTags = ref([]);
+
+// Function to add or remove tags based on selection
+const toggleTag = (tag) => {
+  if (selectedTags.value.includes(tag)) {
+    selectedTags.value = selectedTags.value.filter((t) => t !== tag);
+  } else {
+    selectedTags.value.push(tag);
+  }
+  addItem();
+};
+
+// Function to remove a specific tag
+const removeTag = (tag) => {
+  selectedTags.value = selectedTags.value.filter((t) => t !== tag);
+};
+
+// Function to filter items based on selected tags
+const filteredItems = computed(() => {
+  if (selectedTags.value.length === 0) {
+    return items;
+  } else {
+    return items.filter((item) => {
+      return (
+        selectedTags.value.includes(item.role) ||
+        selectedTags.value.includes(item.level) ||
+        item.languages.some((language) => selectedTags.value.includes(language))
+      );
+    });
+  }
+});
 </script>
 
 <style scoped>
