243ad54f605827385052a79984940ab6a4d4b70a
DannyHTML <dannyjag@gmail.com>
2023-07-05 22:52:58 +0200

    small img path changes

--- src/Header.vue
+++ src/Header.vue
@@ -2,13 +2,13 @@
   <div class="w-full h-44 bg-primary">
     <img
       class="w-full h-full md:hidden object-fill"
-      src="images/bg-header-mobile.svg"
-      alt=""
+      src="/images/bg-header-mobile.svg"
+      alt="background-image"
     />
     <img
       class="w-full hidden md:block h-full object-fill"
-      src="images/bg-header-desktop.svg"
-      alt=""
+      src="/images/bg-header-desktop.svg"
+      alt="background-image"
     />
   </div>
 </template>
